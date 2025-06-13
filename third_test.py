d43306bfdeb5b93ddd72ff847bd06a844ca6d0ef
Peter Stieg <PeterStieg@gmx.de>
2025-06-13 11:41:09 +0200

    Peter: Added files for third API test regarding content

---
+++ third_test.py
@@ -0,0 +1,80 @@
+import requests
+import json
+import time
+import os
+
+# ### USER DATA
+# Get usernames and passwords from JSON file
+try:
+    with open("users.json", "r", encoding="utf-8") as user_file:
+        users = json.load(user_file)
+except FileNotFoundError:
+    print("File with registered users not found. Please check the file path.")
+    users = {}
+
+# ### USER DATA
+# Get usernames and passwords from JSON file
+try:
+    with open("content.txt", "r", encoding="utf-8") as content_file:
+        lines = content_file.readlines()
+        # Remove newline characters
+        sentences = [line.strip() for line in lines]
+except FileNotFoundError:
+    print("File with content. Please check the file path.")
+    sentences = ""
+
+
+# ### TIME CONFIGURATION
+# Get current time
+current_time = time.localtime()
+
+# Format time string: Day. Month Year: Hours:Minutes:Seconds
+formatted_time = time.strftime("%d. %B %Y: %H:%M:%S", current_time)
+
+
+# ### API CONFIGURATION
+# API address
+api_address = os.environ.get("API_HOST", "http://172.17.0.1")
+
+# API port
+api_port = 8000
+
+# API URL for permissions
+api_url = f"{api_address}:{api_port}/permissions"
+
+for username, password in users.items():
+
+    # Request to the API
+    r = requests.get(url=api_url, params={"username": username, "password": password})
+
+    # Get status code
+    status_code = r.status_code
+
+    # Display test status
+    if status_code == 200:
+        test_status = "SUCCESS"
+    else:
+        test_status = "FAILURE"
+
+    permissions = r.json().get("permissions", "No permissions found")
+
+    output = f"""
+    ================================================
+     Content test // {formatted_time}
+    ================================================
+    request done at "/permissions"
+    | username = "{username}"
+    | password = "{password}"
+    | permissions = "{permissions}"
+    | content = "{sentences}"
+    expected result = 200
+    actual restult = {status_code}
+    ==>  {test_status}
+    """
+
+    print(output)
+
+    # Print output in log file
+    if os.environ.get("LOG", "").lower() in ["1", "true", "yes"]:
+        with open("/app/logs/api_test.log", "a") as file:
+            file.write(output)
